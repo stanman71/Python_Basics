51bb85e8107013e14e5b0329fe26bfbef7f6c2c9
stanman71 <stanlay@gmx.net>
2019-07-08 15:16:23 +0200

    add flask mobility

---
+++ Flask/Mobility/example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from flask import Flask, render_template
+from flask_mobility import Mobility
+from flask_mobility.decorators import mobile_template
+
+
+app = Flask(__name__)
+Mobility(app)
+
+@app.route('/')
+@mobile_template('{mobile/}index.html')
+def index(template):
+    return render_template(template)
+
+if __name__ == '__main__':
+    app.run("0.0.0.0")
